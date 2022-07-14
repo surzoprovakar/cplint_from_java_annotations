6a8599c0f36322f3f74f189cdb70744e1ef559d2
Provakar Mondal <provakar@vt.edu>
2022-07-14 19:18:33 -0400

    keep coin.pl as comment

--- Probability.java
+++ Probability.java
@@ -1,23 +1,31 @@
 import Annotations.*;
 
-
 public class Probability {
     static {
 		System.loadLibrary("PrologFromCpp");	
 		probabilityValue = probIncident();
 	}
 
+	// region alarm.pl
+	@Event(name = "burglary", probValue = 10)
+	@Event(name = "earthquake", probValue = 20)
 
-	@Event(name="burglary", probValue=10)
-	@Event(name="earthquake", probValue=20)
-
-	@Outcome(conditionalEvents={@Event(name="burglary"), @Event(name="earthquake")}, incidentName="alarm", probValue=100)
-	@Outcome(conditionalEvents={@Event(name="burglary"), @Event(name="!earthquake")}, incidentName="alarm", probValue=80)
-	@Outcome(conditionalEvents={@Event(name="!burglary"), @Event(name="earthquake")}, incidentName="alarm", probValue=80)
-	@Outcome(conditionalEvents={@Event(name="!burglary"), @Event(name="!earthquake")}, incidentName="alarm", probValue=10)
+	@Outcome(conditionalEvents = {@Event(name = "burglary"), @Event(name = "earthquake")}, incidentName = "alarm", probValue = 100)
+	@Outcome(conditionalEvents = {@Event(name = "burglary"), @Event(name = "!earthquake")}, incidentName = "alarm", probValue = 80)
+	@Outcome(conditionalEvents = {@Event(name = "!burglary"), @Event(name = "earthquake")}, incidentName = "alarm", probValue = 80)
+	@Outcome(conditionalEvents = {@Event(name = "!burglary"), @Event(name = "!earthquake")}, incidentName = "alarm", probValue = 10)
 	
     @GetProbability(incidentName="alarm")
+	// endregion
 
+	// region coin.pl
+	// @Event(name = "fair", probValue = 90)
+
+	// @Outcome(conditionalEvents = {@Event(name = "fair")}, incidentName = "head", probValue = 50)
+	// @Outcome(conditionalEvents = {@Event(name = "!fair")}, incidentName = "head", probValue = 60)
+
+	// @GetProbability(incidentName = "head")
+	//endregion
 
 
 	public static native float probIncident();
